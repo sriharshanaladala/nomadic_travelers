be91b67814f42cf2ff00236040a4d5a34a26ec3a
sriharsha <sriharshanaladala999@gmail.com>
2023-05-03 08:38:25 +0530

    creating templates and rendering templates

--- nomadtravelers/nomads/urls.py
+++ nomadtravelers/nomads/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('nomads/', views.nomads, name="nomads"),
+    path('', views.nomads, name="nomads"),
     path('nomad/<str:pk>/', views.nomad, name="project"),
 ]
--- nomadtravelers/nomads/views.py
+++ nomadtravelers/nomads/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def nomads(request):
-    return HttpResponse('here we can se our nomads')
+    msg="hello you are in the project page"
+    return render(request,'nomads/nomads.html',{'msg':msg})
+
+
 def nomad(request,pk):
-    return HttpResponse("single user"+''+ str(pk))
+    return render(request,'nomads/nomad.html')
